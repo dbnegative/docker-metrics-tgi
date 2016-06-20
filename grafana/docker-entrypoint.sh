6bddc6fb993eabfbbfb0ca17dbe676070a2a3907
Jason Witting <jason.witting@MacBook-Pro-2.local>
2016-06-20 21:19:28 +0100

    Added Telegraf and created mock base cpu graph

--- grafana/docker-entrypoint.sh
+++ grafana/docker-entrypoint.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-/opt/grafana/bin/grafana-server web & 
+/opt/telegraf/usr/bin/telegraf --config /opt/telegraf/etc/telegraf/telegraf.conf &
+/opt/grafana/bin/grafana-server web &
 
-sleep 5 
+sleep 5
 
-curl 'http://admin:admin@localhost:3000/api/datasources' -X POST -H 'Content-Type: application/json;charset=UTF-8' --data-binary '{"name":"influx","type":"influxdb","url":"http://influxdb:8086","access":"proxy","isDefault":true,"database":"mydb","user":"admin","password":"admin"}' > /dev/null
+curl 'http://admin:admin@localhost:3000/api/datasources' -X POST -H 'Content-Type: application/json;charset=UTF-8' --data-binary '{"name":"influx","type":"influxdb","url":"http://influxdb:8086","access":"proxy","isDefault":true,"database":"telegraf","user":"admin","password":"admin"}' > /dev/null
+curl -X POST -i 'http://admin:admin@localhost:3000/api/dashboards/db' --data-binary '@/opt/grafana/grafana_dash.json' -H "Content-Type: application/json" -H "Accept: application/json"
 
 wait
