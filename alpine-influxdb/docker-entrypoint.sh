be68bee7db2a6d59936572d62bf12a158baba731
Jason Witting <jason.witting@MacBook-Pro-4.local>
2016-06-29 11:39:57 +0100

    Improved docker scripts.

--- alpine-influxdb/docker-entrypoint.sh
+++ alpine-influxdb/docker-entrypoint.sh
@@ -3,8 +3,8 @@
 /usr/bin/telegraf --config /etc/telegraf/telegraf.conf &
 influxd --config /etc/influxdb.toml & 
 
-sleep 10 
+sleep 15 
 
-curl -POST 'http://localhost:8086/query?u=admin&p=admin' --data-urlencode 'q=CREATE DATABASE mydb'
+curl -POST 'http://localhost:8086/query?u=admin&p=admin' --data-urlencode 'q=CREATE DATABASE telegraf'
 
 wait
